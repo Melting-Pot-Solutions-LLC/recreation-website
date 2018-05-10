6fcf01c71920cc99889c8cfc150a13f6a98a2fdc
Rockman1233 <rocki67@me.com>
2018-05-11 00:54:11 +0300

    10.09

--- js/message.js
+++ js/message.js
@@ -22,8 +22,37 @@ myform.submit(function(event){
         });})
 
 $(document).ready(function() {
-    $("#my_form").submit(function(e) {
-        $("#my_form").hide('slow');
+    $("#my_form2").submit(function(e) {
+        $("#my_form2").hide('slow');
+        $("#main-nav").show();
+    });
+});
+(function(){
+    emailjs.init("user_fHCAuIg6awgTzhY28Nase");
+})();
+var myform = $("form#my_form2");
+myform.submit(function(event){
+    event.preventDefault();
+    var params = myform.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+    // Change to your service ID, or keep using the default service
+    var service_id = "gmail";
+    var template_id = "recreation";
+    myform.find("button").text("Sending...");
+    emailjs.send(service_id,template_id,params)
+        .then(function(){
+            alert("Sent!");
+            myform.find("button").text("Send");
+        }, function(err) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+            myform.find("button").text("Send");
+        });})
+
+$(document).ready(function() {
+    $("#my_form2").submit(function(e) {
+        $("#my_form2").hide('slow');
         $("#main-nav").show();
     });
 });
